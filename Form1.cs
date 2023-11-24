e1615b88f0cef35fceaf95a88e45220f4f3d48e2
PhamTrung1204 <147811633+PhamTrung1204@users.noreply.github.com>
2023-11-24 15:10:17 +0700

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,22 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace Đồ_án_môn_DSA
 {
     public partial class Form1 : Form
     {
-        private LinkedList<string> playlist = new LinkedList<string>();
-        private LinkedListNode<string> currentSongNode;
+        private DoublyLinkedList<string> playlist = new DoublyLinkedList<string>();
+        private Node<string> currentSongNode;
 
         public Form1()
         {
             InitializeComponent();
             progressBar1.MouseClick += progressBar1_Click;
         }
+
         string[] files, paths;
+
         private void lst_List_SelectedIndexChanged(object sender, EventArgs e)
         {
             lbl_MediaPlayer.Text = "Preparing...";
@@ -35,6 +36,7 @@ namespace Đồ_án_môn_DSA
         {
 
         }
+
         private void PlaySelectedSong()
         {
             if (lst_List.SelectedIndex >= 0 && lst_List.SelectedIndex < lst_List.Items.Count)
@@ -42,7 +44,7 @@ namespace Đồ_án_môn_DSA
                 Player.URL = paths[lst_List.SelectedIndex];
                 Player.Ctlcontrols.play();
                 lbl_MediaPlayer.Text = "Playing...";
-                currentSongNode = playlist.Find(paths[lst_List.SelectedIndex]);
+                currentSongNode = playlist.Find(paths[lst_List.SelectedIndex]); 
             }
         }
 
@@ -50,18 +52,18 @@ namespace Đồ_án_môn_DSA
         {
             Player.settings.volume = trackBar1.Value;
             lbl_Percent.Text = trackBar1.Value.ToString() + "%";
-
         }
+
         private void progressBar1_Click(object sender, EventArgs e)
         {
 
         }
+
         private void btn_Pause_Click(object sender, EventArgs e)
         {
             Player.Ctlcontrols.pause();
             lbl_MediaPlayer.Text = "Pause";
         }
-
 
         private void btn_Next_Click(object sender, EventArgs e)
         {
@@ -73,7 +75,7 @@ namespace Đồ_án_môn_DSA
             if (currentSongNode != null && currentSongNode.Next != null)
             {
                 currentSongNode = currentSongNode.Next;
-                lst_List.SelectedIndex = playlist.ToList().IndexOf(currentSongNode.Value);
+                lst_List.SelectedIndex = playlist.IndexOf(currentSongNode.Data);
                 PlaySelectedSong();
             }
             else if (currentSongNode != null && currentSongNode.Next == null)
@@ -81,14 +83,12 @@ namespace Đồ_án_môn_DSA
                 currentSongNode = currentSongNode.Next;
             }
         }
+
         private void btn_Previous_Click_1(object sender, EventArgs e)
         {
             if (lst_List.SelectedIndex > 0)
             {
-                // Giảm chỉ mục của bài hát hiện tại
                 lst_List.SelectedIndex = lst_List.SelectedIndex - 1;
-
-                // Phát bài hát mới
                 PlaySelectedSong();
             }
         }
@@ -102,36 +102,38 @@ namespace Đồ_án_môn_DSA
                 playlist.Clear();
                 Player.Ctlcontrols.stop();
                 lbl_MediaPlayer.Text = "Stop";
-                timer1.Stop(); // Dừng đồng hồ
+                timer1.Stop();
                 progressBar1.Value = 0;
                 lbl_Start.Text = "00:00";
                 lbl_End.Text = "00:00";
             }
-
         }
-
-
 
         private void btn_Tuanhanh_Click(object sender, EventArgs e)
         {
             Player.Ctlcontrols.currentPosition -= 10;
         }
+
         private void btn_Tuavesau_Click(object sender, EventArgs e)
         {
             Player.Ctlcontrols.currentPosition += 10;
         }
+
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
+
         private void lbl_Percent_Click(object sender, EventArgs e)
         {
 
         }
+
         private void lbl_Start_Click(object sender, EventArgs e)
         {
 
         }
+
         private void timer1_Tick(object sender, EventArgs e)
         {
             if (Player.playState == WMPLib.WMPPlayState.wmppsPlaying)
@@ -147,11 +149,9 @@ namespace Đồ_án_môn_DSA
 
                 TimeSpan totalDurationTime = TimeSpan.FromSeconds(totalDuration);
                 lbl_End.Text = string.Format("{0:D2}:{1:D2}", totalDurationTime.Minutes, totalDurationTime.Seconds);
-
             }
             else if (Player.playState == WMPLib.WMPPlayState.wmppsStopped)
             {
-                // Nếu bài hát kết thúc, tự động chuyển đến bài hát tiếp theo
                 PlayNextSong();
             }
         }
@@ -165,6 +165,11 @@ namespace Đồ_án_môn_DSA
             PlaySelectedSong();
         }
 
+        private void btn_Remove_Click(object sender, EventArgs e)
+        {
+
+        }
+
         private void btn_Open_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
@@ -174,16 +179,113 @@ namespace Đồ_án_môn_DSA
                 files = ofd.SafeFileNames;
                 paths = ofd.FileNames;
 
-                // Thêm từng bài hát vào danh sách liên kết
                 for (int i = 0; i < files.Length; i++)
                 {
-                    // Thêm vào danh sách liên kết
                     playlist.AddLast(paths[i]);
-
-                    // Thêm vào danh sách hiển thị
                     lst_List.Items.Add(files[i]);
                 }
             }
         }
+    }
+
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+        public Node<T> Previous { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
+            Previous = null;
+        }
+    }
+
+    public class DoublyLinkedList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public void AddLast(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+
+            if (head == null)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                newNode.Previous = tail;
+                tail.Next = newNode;
+                tail = newNode;
+            }
+        }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+        }
+
+        public Node<T> Find(T data)
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                if (EqualityComparer<T>.Default.Equals(current.Data, data))
+                {
+                    return current;
+                }
+                current = current.Next;
+            }
+            return null;
+        }
+
+        public int IndexOf(T data)
+        {
+            int index = 0;
+            Node<T> current = head;
+            while (current != null)
+            {
+                if (EqualityComparer<T>.Default.Equals(current.Data, data))
+                {
+                    return index;
+                }
+                current = current.Next;
+                index++;
+            }
+            return -1;
+        }
+        /*public void RemoveSong(string path)
+        {
+            Node<string> nodeToRemove = Find(path);
+
+            if (nodeToRemove != null)
+            {
+                // Xóa nút khỏi danh sách
+                if (nodeToRemove.Previous != null)
+                {
+                    nodeToRemove.Previous.Next = nodeToRemove.Next;
+                }
+                else
+                {
+                    // Nếu nút cần xóa là nút đầu tiên trong danh sách
+                    head = nodeToRemove.Next;
+                }
+
+                if (nodeToRemove.Next != null)
+                {
+                    nodeToRemove.Next.Previous = nodeToRemove.Previous;
+                }
+                else
+                {
+                    // Nếu nút cần xóa là nút cuối cùng trong danh sách
+                    tail = nodeToRemove.Previous;
+                }
+            }
+        }*/
     }
 }
